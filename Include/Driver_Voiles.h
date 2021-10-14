d0b05999c77d4e56128d5999f7da343da337fa08
Manah7 <manah@gmx.com>
2021-10-14 10:49:13 +0200

    implémentation du driver voiles (pré-version)

--- Include/Driver_Voiles.h
+++ Include/Driver_Voiles.h
@@ -3,16 +3,21 @@
 
 #include "stm32f10x.h"
 
+#include "Driver_Timer.h"
+#include "Driver_GPIO.h"
+
 /*
 Driver pour le réglage des voiles
 	PINs utilisés :
 		- PA6
 		Timer utilisé :
-		- TIM3
-		- TIM4 ?
+		- TIM3, channel 1 (correspond à PA6)
 */
 
-/* Réglage des voiles, de 0 à 255, 255 étant le plus bordé */
+/* Initialise le module voiles. Regle les voiles en position 0. */
+void Init_Voiles(void);
+
+/* Réglage des voiles, de 0 à 5 (inclus), 5 étant le plus bordé */
 void Regler_Voiles(short r);
 
 #endif 
--- Src/Driver_Voiles.c
+++ Src/Driver_Voiles.c
@@ -1,1 +1,52 @@
 #include "Driver_Voiles.h"
+
+#define CHANNEL 1
+
+MyTimer_Struct_TypeDef timerVoiles;
+MyGPIO_Struct_TypeDef pa6;
+
+/* 
+Fonction interne. Ajuste un short pour correspondre à
+une valeur entre 5 et 10 % (entre 1 et 2 ms, cycle 20ms).
+*/ 
+int Convert_to_Pc(short v) {
+	return (int) v + 5;
+}
+
+/*
+Initialisation du timer pour la PWM du module
+voiles. Les parametres de la PWN sont les suivants :
+	- Une pulsation toutes les 20 ms
+	- La durée d'impulsion est comprise entre 1 et 2 ms
+	
+Calcul du PSC et ARR :
+	T_timer = T_horloge * prescaler[1 | 2^16] * autoreload
+	autoreload = T_timer / (prescaler * T_horloge)
+avec :
+	T_horloge = 1/72M = 1/72000000
+	T_Timer = 20 ms = 0.02
+	0 < autoreload < 65535
+	0 < prescaler < 65535
+	
+Application des valeurs :
+	ARR = 1381
+	PSC = 1042
+*/
+void Init_Voiles(void) {
+	timerVoiles.Timer = TIM3;
+	timerVoiles.ARR = 1381;
+	timerVoiles.PSC = 1042;
+	
+	MyTimer_Base_Init(&timerVoiles);
+	MyTimer_Base_Start(timerVoiles.Timer);
+	
+	MyTimer_PWM(timerVoiles.Timer, CHANNEL);
+	Set_pwm_percentage(timerVoiles.Timer, Convert_to_Pc(0), CHANNEL);
+}
+
+/*
+	Regle la PWM entre 5% (1ms) et 10% (2ms)
+*/
+void Regler_Voiles(short r) {
+	Set_pwm_percentage(timerVoiles.Timer, Convert_to_Pc(r), CHANNEL);
+}
