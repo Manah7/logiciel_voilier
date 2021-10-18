a912fe2ab1a6c0e15ed4dbd0230bddbacfc42bfc
Enjmateo <enjmateo@yahoo.fr>
2021-10-18 13:27:06 +0200

    Ajout de la gestion de la réception des messages en UART

--- Include/Driver_Communication.h
+++ Include/Driver_Communication.h
@@ -7,15 +7,14 @@
 //Configure l'UART
 void Config_UART(USART_TypeDef * UART_Par);
 
-//Configure l'envoi de message
-void Init_Message_Sending(int delay);
+//Configure l'envoi de messages
+//void Init_Message_Sending(int delay);
 
-//Configuerer la réception de messages
-//void Init_Message_Receiving();
-
-//Envoi de messages
+//Envoi de message
 void Send_Message(const char * message[]);
 
+//Configure la reception des messages (prend en argument la fonction appelée à la réception avec en paramètre le caractère reçu)
+void Init_Message_Receiving(void (* ptrFonction) (char));
 
 
 #endif
