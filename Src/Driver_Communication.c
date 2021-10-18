537c80fb1e3e4bac78fd118b00c047c4e121d4a8
Enjmateo <enjmateo@yahoo.fr>
2021-10-18 14:51:34 +0200

    Corrections UART (ajout des GPIOs)

--- Src/Driver_Communication.c
+++ Src/Driver_Communication.c
@@ -4,14 +4,46 @@ USART_TypeDef * UART;
 void (* pFncUART) (char);
 
 void SendChar(char character){
-	//Tant que le transmission précédente n'est pas finit (tant que le bit CT de SR est à 0)
+	//Tant que la transmission précédente n'est pas finit (tant que le bit TC de SR est à 0)
 	while(((UART->SR>>6)&0x1)==0x0);
 	UART->DR = character;
 }
 
 void Config_UART(USART_TypeDef * UART_par){
-	// On met à 0 le bit M (12) de CR1 pour définir la taille des mots à 8
+	MyGPIO_Struct_TypeDef gpioRX, gpioTX;
+	
 	UART = UART_par;
+
+	//On configure les GPIO :	
+	gpioTX.GPIO_Conf = AltOut_Ppull;
+	gpioRX.GPIO_Conf = AltOut_Ppull;
+	switch ((int)UART){
+				case (int)USART2:
+					gpioTX.GPIO = GPIOA;
+					gpioTX.GPIO_Pin = 2;
+					gpioRX.GPIO = GPIOA;
+					gpioRX.GPIO_Pin = 3;
+				break;
+				case (int)USART3:
+					gpioTX.GPIO = GPIOB;
+					gpioTX.GPIO_Pin = 10;
+					gpioRX.GPIO = GPIOB;
+					gpioRX.GPIO_Pin = 11;
+				break;
+	}
+	MyGPIO_Init(&gpioTX);
+	MyGPIO_Init(&gpioRX);
+
+	//On active la clock :
+	switch ((int)UART){
+		case ((int)USART2) : 
+			RCC->APB1ENR |= (0x1 <<17);
+		break;
+		case ((int)USART3) : 
+			RCC->APB1ENR |= (0x1 << 18);
+		break;
+	}
+	// On met à 0 le bit M (12) de CR1 pour définir la taille des mots à 8
 	UART->CR1 &= ~(0x1 << 12); 
 	
 	// On met à 1  le bit UE (13) de CR1 pour activer l'UART
@@ -29,11 +61,14 @@ void Config_UART(USART_TypeDef * UART_par){
 	
 	//On mets à 1 le bit TE de CR1 pour envoyer une frame de première transmission
 	UART->CR1 |= (0x1 << 3);
+	
+	UART->SR |= (0x1 <<6);
 }
-void Send_Message(const char* message[]){
+void Send_Message(const char message[]){
 	int i = 0;
 	do{
 		SendChar((char)message[i]);
+		i++;
 	}while( message[i]!='\0');
 }
 
@@ -57,28 +92,20 @@ void Init_Message_Receiving(void (* ptrFonction) (char)){
 void USART2_IRQHandler(){
 	//Si le bit RXNE de SR est à 1 (on a bien reçu une donnée) on appelle la fonction
 	if((UART->SR>>6)&0x1){
-		//On remet le bit à 0
-		UART->SR &= ~(0x1<<6);
 		//On récupère le caratère dans DR
 		pFncUART(UART->DR);
+		//On remet le bit à 0
+		UART->SR &= ~(0x1<<6);
 	}
 }
 void USART3_IRQHandler(){
 	//Si le bit RXNE de SR est à 1 (on a bien reçu une donnée) on appelle la fonction
 	if((UART->SR>>6)&0x1){
-		//On remet le bit à 0
-		UART->SR &= ~(0x1<<6);
 		//On récupère le caratère dans DR
 		pFncUART(UART->DR);
-	}
-}
-void USART4_IRQHandler(){
-	//Si le bit RXNE de SR est à 1 (on a bien reçu une donnée) on appelle la fonction
-	if((UART->SR>>6)&0x1){
 		//On remet le bit à 0
 		UART->SR &= ~(0x1<<6);
-		//On récupère le caratère dans DR
-		pFncUART(UART->DR);
 	}
 }
+
 
