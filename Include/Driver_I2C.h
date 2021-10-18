8283fe13a7895b3d8c6f524d43d886c43850937e
Noecv <chauveanpro@gmail.com>
2021-10-18 15:19:05 +0200

    I2C : la suite

--- Include/Driver_I2C.h
+++ Include/Driver_I2C.h
@@ -2,13 +2,16 @@
 #define I2C_H
 
 #include "stm32f10x.h"
+#include "Driver_GPIO.h"
 
 /* Besoin de deux pins : PB6(SCL) et PB7(SDA) */
 
 /* Initialise le I2C des ports PB6/PB7
-avec l'adresse du periphérique 1 et 2 */
-void I2C_Init(uint32_t ClockSpeed, uint32_t OwnAddress1, uint32_t AddressingMode, uint32_t OwnAddress2);
+avec l'adresse du periphérique 1 */
+void I2C_Init(uint32_t ClockSpeed, uint32_t OwnAddress1);
 
-void I2C_Adress();
+/*Renvoie le contenu stocké dans le registre d'addresse Register_address
+du périphérique d'addresse Device_address*/
+uint32_t I2C_getdata(uint32_t Device_address, uint32_t Register_address);
 
 #endif
--- Src/Driver_I2C.c
+++ Src/Driver_I2C.c
@@ -1,1 +1,32 @@
 #include "Driver_I2C.h"
+
+void I2C_Init(uint32_t ClockSpeed, uint32_t OwnAddress) {
+	
+	// Reset des registres I2C
+	RCC->APB1RSTR |=  RCC_APB1RSTR_I2C1RST;
+	RCC->APB1RSTR &= ~RCC_APB1RSTR_I2C1RST;
+	I2C1->CR1 |= I2C_CR1_SWRST; 
+	I2C1->CR1 &= ~I2C_CR1_SWRST; 
+	
+	// Initialisation des GPIO
+	MyGPIO_Struct_TypeDef GPIO_SCL;
+	MyGPIO_Struct_TypeDef GPIO_SDA;
+	GPIO_SCL.GPIO = GPIOB;
+	GPIO_SCL.GPIO_Pin = 6;
+	GPIO_SCL.GPIO_Conf = AltOut_Ppull;
+	GPIO_SDA.GPIO = GPIOB;
+	GPIO_SDA.GPIO_Pin = 7;
+	GPIO_SDA.GPIO_Conf = AltOut_Ppull;
+	
+	MyGPIO_Init(&GPIO_SCL);
+	MyGPIO_Init(&GPIO_SDA);
+	
+	
+	
+	RCC->APB1ENR |=RCC_APB1ENR_I2C1EN;
+	
+	I2C1->CR1 |= I2C_CR1_PE;     // Peripheral Enable
+	I2C1->CR2 |= I2C_CR2_FREQ_1;
+  I2C1->CCR |= ClockSpeed;
+	//I2C1->OAR1 |= OwnAddress;
+}
