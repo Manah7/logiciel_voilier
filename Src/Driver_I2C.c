8ae87510535d59b07885335c6d6b667484697343
Enjmateo <enjmateo@yahoo.fr>
2021-11-09 17:22:30 +0100

    I2C readbytes générique

--- Src/Driver_I2C.c
+++ Src/Driver_I2C.c
@@ -33,13 +33,44 @@ void I2C_Init(uint32_t ClockSpeed, uint32_t OwnAddress) {
 	//I2C1->OAR1 |= OwnAddress;
 }
 
-void I2C_START(uint16_t mode, uint16_t device_address) {
-	while((I2C1->SR1 & I2C_SR2_BUSY)!=0x0000) {}
-	I2C1->CR1 |= I2C_CR1_START;
-	while ((I2C1->SR1 & I2C_SR1_SB) != 0x0000){}
-	I2C1->DR |= (RTC_ADDRESS <<1) | mode;  //On met l'adresse dans la mémoire et on ajoute 1 a droite pour se placer en mode lecture 
+void I2C_getBytes(char Register_address, char Device_adress, char * tab){
+	//On envoie un start
+	I2C_START(0, Device_adress);
+	//On mets l'adresse du registre dans DR (sans toucher au bits réservés)
+	I2C1->DR = (I2C1->DR & 0xFF00) | (0xFF & Register_address);
+	//On attend un ack
 	while ((I2C1->SR1 & I2C_SR1_ADDR) == 0x0000) {}
-	I2C1->SR1 &= (~I2C_SR1_ADDR); 
+	//On fait un stop puis un start en lecture
+	I2C1->CR1 |= I2C_CR1_STOP;
+	I2C_START(1, Device_adress);
+	//On attend l'ACK
+	while ((I2C1->SR1 & I2C_SR1_ADDR) == 0x0000) {}
+	
+	//On active l'ACK automatique
+	I2C1->CR1 |=  I2C_CR1_ACK;
+
+		for(int i=0 ; i<sizeof(tab);i++){
+			//Si c'est le dernier byte on envoie un NACK
+			if(i==sizeof(tab)-1) I2C1->CR2 |=  I2C_CR2_LAST;
+			//Attente de la réception d'une donnée
+			while((I2C1->SR1 & I2C_SR1_BTF)==0) {}
+				tab[i]=(char)I2C1->DR;
+		}
+		
+		//On envoie un stop 
+		I2C1->CR1 |= I2C_CR1_STOP;
+		
+}
+
+
+
+void I2C_START(uint16_t mode, uint16_t device_address) {
+	while((I2C1->SR1 & I2C_SR2_BUSY)!=0x0000) {} //Bus libre?
+	I2C1->CR1 |= I2C_CR1_START; //Génération bit start
+	while ((I2C1->SR1 & I2C_SR1_SB) != 0x0000){} //bit start bien passé?
+	I2C1->DR |= (RTC_ADDRESS <<1) | mode;  //On met l'adresse dans la mémoire et on ajoute 1 a droite pour se placer en mode lecture 
+	while ((I2C1->SR1 & I2C_SR1_ADDR) == 0x0000) {} //Attente ACK
+	I2C1->SR1 &= (~I2C_SR1_ADDR); //Fin de l'adressage 
 	
 }
 
--- Src/Driver_IMU.c
+++ Src/Driver_IMU.c
@@ -1,1 +1,14 @@
 #include "Driver_IMU.h"
+#include "Driver_I2C.h"
+
+
+void read(){	char valeurs[6];
+	I2C_getBytes(0x32,IMU_ADDRESS,valeurs);
+}
+
+
+void Initialize_IMU(){
+	I2C_Init(100000, 1);
+
+	
+}
