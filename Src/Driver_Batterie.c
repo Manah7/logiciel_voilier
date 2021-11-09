2e5a8e56ae586ac4fc8b5df34c19f38485ae53df
Hennrick Pelloux <pelloux@insat.insa-toulouse.fr>
2021-11-09 16:42:49 +0100

    driver batterie en cours

--- Src/Driver_Batterie.c
+++ Src/Driver_Batterie.c
@@ -4,9 +4,17 @@ MyGPIO_Struct_TypeDef gpio;
 
 void Init_Batterie(void)
 {
+	gpio.GPIO = GPIOB;
+	gpio.GPIO_conf = In_Analog;
+	gpio.GPIO_Pin = 0;
+	MyGPIO_Init(&gpio);
 	
+	configADC1(8);
 }
 
-void Get_Valeur_Batterie(void);
+int Get_Valeur_Batterie(void)
+{
+	return getADC1VALUE();
+}
 
 int Is_Batterie_Faible(int valeur);
