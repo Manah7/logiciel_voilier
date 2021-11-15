b4969a8310f9225b7f7846aef9470c83a5cdffe8
Hennrick Pelloux <pelloux@insat.insa-toulouse.fr>
2021-11-15 10:05:20 +0100

    implementation du driver batterie (utiliser is_batterie_faible)

--- Include/Driver_Batterie.h
+++ Include/Driver_Batterie.h
@@ -4,6 +4,9 @@
 #include "Driver_ADC.h"
 #include "Driver_GPIO.h"
 
+#define V_BAT 12
+#define VALEUR_MAX_ADC 4095
+
 /*
 	Driver pour la gestion de la batterie
 	Utilise les memes PINs que le driver de l'ADC
--- Src/Driver_Batterie.c
+++ Src/Driver_Batterie.c
@@ -14,7 +14,14 @@ void Init_Batterie(void)
 
 int Get_Valeur_Batterie(void)
 {
-	return getADC1Value() * 13;
+	return (int) ((getADC1Value() * 12)/4095);
 }
 
-int Is_Batterie_Faible(int valeur);
+int Is_Batterie_Faible(int valeur)
+{
+	// l'ADC retournant une valeur sur 12 bits,
+	// on se retrouve avec des valeurs comprises entre
+	// 0 et 4095 (2^12=4096)
+	// donc on a vBatt = (valADC * 12)/4095
+	return ((getADC1Value() * V_BAT) / VALEUR_MAX_ADC) < 11.0;
+}
